277c2aad7f44eec3c5ae08148a5b523e834746a8
ravi4all <ravikanttyagi.2295@gmail.com>
2017-09-30 19:59:36 +0530

    Add files via upload

--- CorePythonAgain/Applications/03-KBC/main.py
+++ CorePythonAgain/Applications/03-KBC/main.py
@@ -1,9 +1,13 @@
+import time
+
 options = [['Modi','Arvind','Yogi','Lalu'],
            ['Mumbai', 'Delhi', 'Lucknow', 'Goa'],
            ['Global Service Tax', 'Goods and Simple Tax', 'Goods and Services Tax', 'General Service Tax'],
            ['1000','200','50','400']]
 
-answers = ['Modi','Delhi','Goods and Simple Tax',200]
+money = [1000,5000,10000,50000]
+
+answers = ['Modi','Delhi','Goods and Simple Tax',200,'Delhi']
 
 with open('ques.txt') as file:
     qs = file.readlines()
@@ -15,7 +19,15 @@ with open('ques.txt') as file:
 
             ans = input("Enter answer : ")
 
-            if ans in answers:
+            if ans == answers[i]:
+                time.sleep(3)
                 print("Corrent Answer...")
+                print("You won",money[i])
             else:
+                time.sleep(3)
                 print("Wrong Answer...")
+                print("Total Amount",money[i-1])
+                break
+        break
+
+print("Game Over...")
