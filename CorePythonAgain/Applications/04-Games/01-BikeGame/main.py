a3eec34e873982fa1a869c72e20931114a8d74b4
ravi4all <ravikanttyagi.2295@gmail.com>
2017-10-06 13:25:29 +0530

    Add files via upload

--- CorePythonAgain/Applications/04-Games/01-BikeGame/main.py
+++ CorePythonAgain/Applications/04-Games/01-BikeGame/main.py
@@ -17,6 +17,22 @@ backgroundY2 = -height
 
 moveBackground = 10
 
+myCar = pygame.image.load("assets/car_1.png")
+myCarRect = myCar.get_rect()
+oppCar_1 = pygame.image.load("assets/car_2.png")
+oppCar_1Rect = oppCar_1.get_rect()
+oppCar_2 = pygame.image.load("assets/car_3.png")
+oppCar_2Rect = oppCar_2.get_rect()
+
+carX = width/2
+carY = height - 140
+
+oppCar_1X = width/2 - 70
+oppCar_1Y = height - 140
+
+oppCar_2X = width/2 + 70
+oppCar_2Y = height - 140
+
 clock = pygame.time.Clock()
 FPS = 30
 
@@ -30,8 +46,21 @@ while True:
             pygame.quit()
             quit()
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                moveBackground += 5
+            elif event.key == pygame.K_DOWN:
+                moveBackground -= 5
+        # if event.type == pygame.KEYUP:
+        #     moveBackground -= 10
+
+    screen.blit(backgroundImage, (0,0))
     screen.blit(backgroundImage, (0,backgroundY))
     screen.blit(backgroundImage2, (0,backgroundY2))
+
+    screen.blit(myCar, (carX, carY))
+    screen.blit(oppCar_1, (oppCar_1X, oppCar_1Y))
+    screen.blit(oppCar_2, (oppCar_2X, oppCar_2Y))
 
     backgroundY += moveBackground
     backgroundY2 += moveBackground
